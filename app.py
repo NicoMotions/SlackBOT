e887749a33f433e6cfb6eddfbd5d40b00444d347
NicoMotions <nico@gilly.cc>
2025-03-14 17:36:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -84,5 +84,6 @@ def slack_events():
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.getenv("PORT", 3000))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 3000)))
+
 
